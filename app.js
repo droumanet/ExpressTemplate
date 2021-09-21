2507eef467f9f81bdf27914020abe27c86cbee9d
David ROUMANET <golfy@free.fr>
2021-09-21 23:11:07 +0200

    Version sécurisée de l'application, correction sur les commentaires

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
 /**
  * @author David ROUMANET
- * @version 1.0.0
+ * @version 1.1.0
  * @description Modèle d'application MVC utisant Mongoose, Express, EJS... et un peu de JSDoc
  * La source originale du code est accessible sur https://www.c-sharpcorner.com/blogs/how-to-setup-mvc-design-pattern-in-express
  * Toutefois, le code a été fortement remanié sur certains aspects (pas de callback sur la partie modèle).
+ * Cette version ajoute la sécurisation de l'application : HTTPS et Authentification
  */
 
+// mise en place des modules nécessaires pour l'application
+const fs = require('fs')
+const https = require('https')
 const express  = require('express')  
 const mongoose = require('mongoose')  
 const path     = require('path')  
 const defaultRoutes = require('./routes/defaultRoutes') 
-const medecinsRoutes = require('./routes/medecinsRoutes')   
+const medecinsRoutes = require('./routes/medecinsRoutes')
 
 // connexion à MongoDB (via framework Mangoose), base de données 'demoDB'
 mongoose.connect('mongodb://localhost:27017/Hopital',{useNewUrlParser:true, useUnifiedTopology: true})  
@@ -26,7 +30,7 @@ app.use(express.static("public"))
 // Choix du moteur de rendu des vues : EJS 
 app.set('view engine','ejs')  
 
-//fetch form data from the request
+// Récupération des données encodée en X-WWW-...
 app.use(express.urlencoded({extended: false}))  
 //app.use(bodyParser.urlencoded({extended:false}))  
 
@@ -38,9 +42,17 @@ app.use('/', defaultRoutes)
 app.use('*', defaultRoutes)
 
 // Définition du port de l'application  
-const port = process.env.port || 3000;  
+const port = process.env.port || 3000;
+
+// Définition des certificats pour le protocole HTTPS
+const key = fs.readFileSync(path.join(__dirname, 'certificate', 'server.key'));
+const cert = fs.readFileSync(path.join(__dirname, 'certificate', 'server.cert'));
+const options = { key, cert };
+
 
 // DÉMARRAGE DE L'APPLICATION
-app.listen(port,()=>console.log(`server running at port ${port}`))  
+https.createServer(options, app).listen(port, () => {
+    console.log(`server running HTTPS. Go to https://localhost:${port}`);
+  }); 
 
 module.exports = app;
