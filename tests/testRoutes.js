c5ac9173b2dbb958af5a947d70429411d0dc73f6
prRoumanet <golfy@free.fr>
2025-02-03 09:07:24 +0100

    ajout tests unitaires chai

---
+++ tests/testRoutes.js
@@ -0,0 +1,28 @@
+// test/test-routes.js
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const app = require('../src/app'); // Importer l'application Express
+const expect = chai.expect;
+
+chai.use(chaiHttp);
+
+describe('Routes', () => {
+  it('devrait rendre la page index.ejs', (done) => {
+    chai.request(app)
+      .get('/') // Tester la route GET /
+      .end((err, res) => {
+        expect(res).to.have.status(200); // Vérifier que le statut est 200
+        expect(res.text).to.include('Bienvenue'); // Vérifier que la réponse contient du texte de la page EJS
+        done();
+      });
+  });
+
+  it('devrait retourner une erreur 404 pour une route inexistante', (done) => {
+    chai.request(app)
+      .get('/route-inexistante') // Tester une route qui n'existe pas
+      .end((err, res) => {
+        expect(res).to.have.status(404); // Vérifier que le statut est 404
+        done();
+      });
+  });
+});
